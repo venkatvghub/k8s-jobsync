4b7bd1a9ea799670442d36670e7d51ad2aa96a41
Venkat <venkat.v@razorpay.com>
2018-08-13 13:59:59 +0530

    [controller] - adds annotation constants, annotation checks

--- controller.go
+++ controller.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"reflect"
 	"strconv"
@@ -8,6 +9,7 @@ import (
 	"time"
 
 	v1beta1 "k8s.io/api/apps/v1beta1"
+	batchv1beta1 "k8s.io/api/batch/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/version"
@@ -22,6 +24,20 @@ type DeploymentController struct {
 	deploymentInformer cache.SharedIndexInformer
 	client             *kubernetes.Clientset
 }
+
+//JobStruct ...
+type JobStruct struct {
+	Jobs []string `json:"jobs"`
+}
+
+const (
+	//JobSyncEnabledAnnotation annotation for enabling job sync
+	JobSyncEnabledAnnotation = "jobsync.k8s.io/enabled"
+	//JobSyncedAnnotation annotation for marking deployment as sycned
+	JobSyncedAnnotation = "jobsync.k8s.io/synced"
+	//JobAnnotationPrefix annotation prefix for list of jobs
+	JobAnnotationPrefix = "jobsync.k8s.io/jobs"
+)
 
 // NewDeploymentController creates a new NewDeploymentController
 func NewDeploymentController(client *kubernetes.Clientset, opts map[string]string) *DeploymentController {
@@ -49,12 +65,9 @@ func NewDeploymentController(client *kubernetes.Clientset, opts map[string]strin
 		cache.Indexers{},
 	)
 	deploymentInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(cur interface{}) {
-			deploymentWatcher.deploymentAdded(cur, dryRun, *version)
-		},
 		UpdateFunc: func(old, cur interface{}) {
 			if !reflect.DeepEqual(old, cur) {
-				deploymentWatcher.deploymentAdded(cur, dryRun, *version)
+				deploymentWatcher.deploymentUpdated(cur, dryRun, *version, opts["namespace"])
 			}
 		},
 	})
@@ -81,11 +94,13 @@ func (c *DeploymentController) Run(stopCh <-chan struct{}, wg *sync.WaitGroup) {
 	<-stopCh
 }
 
-func (c *DeploymentController) deploymentAdded(cur interface{}, dryRun bool, version version.Info) {
+func (c *DeploymentController) deploymentUpdated(cur interface{}, dryRun bool, version version.Info, namespace string) {
 	deploymentObj := cur.(*v1beta1.Deployment)
+	isDeploymentAnnotated := c.isDeploymentAnnotated(deploymentObj)
 	isDeploymentAvailable := false
-	if deploymentObj.Status.ObservedGeneration < deploymentObj.Generation {
+	if isDeploymentAnnotated && deploymentObj.Status.ObservedGeneration < deploymentObj.Generation {
 		for _, d := range deploymentObj.Status.Conditions {
+			log.Printf("Deployment Status:%s, Size:%s", d.Status, d.Reason)
 			if d.Status == "True" {
 				isDeploymentAvailable = true
 				break
@@ -93,16 +108,69 @@ func (c *DeploymentController) deploymentAdded(cur interface{}, dryRun bool, ver
 		}
 	}
 	if isDeploymentAvailable == true {
-		log.Printf("Deployment Added:%s", deploymentObj.ObjectMeta.Name)
+		c.applyJobDeployment(deploymentObj, namespace)
 	}
 }
 
-func (c *DeploymentController) deploymentDeleted(cur interface{}, dryRun bool, version version.Info) {
-	deploymentObj := cur.(*v1beta1.Deployment)
-	log.Printf("Deployment Deleted:%s", deploymentObj.ObjectMeta.Name)
+func (c *DeploymentController) isDeploymentAnnotated(deployment *v1beta1.Deployment) bool {
+	_, ok := deployment.ObjectMeta.Annotations[JobSyncEnabledAnnotation]
+	return ok
 }
 
-func (c *DeploymentController) deploymentUpdated(cur interface{}, dryRun bool, version version.Info) {
-	deploymentObj := cur.(*v1beta1.Deployment)
-	log.Printf("Deployment Updated:%s", deploymentObj.ObjectMeta.Name)
+func (c *DeploymentController) getJobsForDeployment(deployment *v1beta1.Deployment) *JobStruct {
+	annotations := deployment.ObjectMeta.Annotations[JobAnnotationPrefix]
+	jobStruct := JobStruct{}
+	json.Unmarshal([]byte(annotations), &jobStruct)
+	return &jobStruct
+}
+
+func (c *DeploymentController) buildJobMap(deployment *v1beta1.Deployment) *map[string]string {
+	jobStruct := c.getJobsForDeployment(deployment)
+	deploymentImage := deployment.Spec.Template.Spec.Containers[0].Image
+	jobs := jobStruct.Jobs
+	jobImageMap := make(map[string]string)
+	for id := range jobs {
+		jobName := jobs[id]
+		jobImageMap[string(jobName)] = string(deploymentImage)
+	}
+	return &jobImageMap
+}
+
+func (c *DeploymentController) applyJobDeployment(deployment *v1beta1.Deployment, namespace string) {
+	log.Printf("Found Deployment %s with Annotation", deployment.Name)
+	jobImageMap := c.buildJobMap(deployment)
+	log.Print("CALLING SYNC_CRON_JOB")
+	c.syncCronJob(*jobImageMap, deployment, namespace)
+	log.Printf("Deployment Occured for Deployment:%s, NameSpace:%s", deployment.Name, deployment.Namespace)
+}
+
+func (c *DeploymentController) syncCronJob(jobs map[string]string, deployment *v1beta1.Deployment, namespace string) error {
+	k8scli := c.client
+	cronList, err := k8scli.BatchV1beta1().CronJobs(namespace).List(metav1.ListOptions{})
+	if err != nil {
+		log.Printf("failed to list cronjobs %v\n", err)
+		return err
+	}
+	for _, cronJob := range cronList.Items {
+		isAnnotated := c.isJobAnnotated(&cronJob)
+		log.Printf("JOB NAME:%s, IS_ANNOTATED:%v", cronJob.GetName(), isAnnotated)
+		if c.isJobAnnotated(&cronJob) {
+			jobName := cronJob.GetName()
+			if image, ok := jobs[jobName]; ok {
+				cronJob.Spec.JobTemplate.Spec.Template.Spec.Containers[0].Image = image
+				log.Printf("IMAGE INSIDE:%s", image)
+				if _, err := k8scli.BatchV1beta1().CronJobs(namespace).Update(&cronJob); err != nil {
+					log.Printf("Image updated on Cronjob:%s, Image:%s, Deployment:%s", jobName, image, deployment.Name)
+					return err
+				}
+				log.Printf("JOB_NAME:%s, UPDATED_TO:%s, DEPLOYMENT:%s", jobName, image, deployment.Name)
+			}
+		}
+	}
+	return nil
+}
+
+func (c *DeploymentController) isJobAnnotated(cronJob *batchv1beta1.CronJob) bool {
+	_, ok := cronJob.ObjectMeta.Annotations[JobSyncEnabledAnnotation]
+	return ok
 }
